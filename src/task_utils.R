df827e3aec1cb7207aa354f234be6fd20afb5c6e
coll nell <collnellphd@gmail.com>
2020-07-21 13:57:56 -0500

    add outputs for twitter, instagram, and facebook

--- src/task_utils.R
+++ src/task_utils.R
@@ -1,14 +1,39 @@
 
-create_frame_plan <- function(gage_years, width, height){
-  step1 <- create_task_step(
-    step_name = 'plot',
-    target_name = function(task_name, step_name, ...) {
-      sprintf('twitter/gage_age_%s.png', task_name)
-    },
-    command = function(task_name, step_name, ...) {
-      sprintf('plot_sites(target_name, gage_melt, state_map = state_map, site_map = site_map, yr = %s, width = %s, height = %s)', task_name, width, height)
-    }
-  )
+create_frame_plan <- function(gage_years, width, height, format){
+  
+  if (format == 'twitter') {
+    step1 <- create_task_step(
+      step_name = 'plot',
+      target_name = function(task_name, step_name, ...) {
+        sprintf('outtwitter/gage_age_%s.png', task_name)
+      },
+      command = function(task_name, step_name, ...) {
+        sprintf('plot_twitter(target_name, gage_melt, state_map = state_map, site_map = site_map, yr = %s, width = %s, height = %s)', task_name, width, height)
+      }
+    )
+  } else if (format == 'instagram') {
+    step1 <- create_task_step(
+      step_name = 'plot',
+      target_name = function(task_name, step_name, ...) {
+        sprintf('out/instagram/gage_age_%s.png', task_name)
+      },
+      command = function(task_name, step_name, ...) {
+        sprintf('plot_instagram(target_name, gage_melt, state_map = state_map, site_map = site_map, yr = %s, width = %s, height = %s)', task_name, width, height)
+      }
+    )
+  } else if (format == 'facebook') {
+    step1 <- create_task_step(
+      step_name = 'plot',
+      target_name = function(task_name, step_name, ...) {
+        sprintf('out/facebook/gage_age_%s.png', task_name)
+      },
+      command = function(task_name, step_name, ...) {
+        sprintf('plot_facebook(target_name, gage_melt, state_map = state_map, site_map = site_map, yr = %s, width = %s, height = %s)', task_name, width, height)
+      }
+    )
+    
+  }
+
   task_plan <- create_task_plan(as.character(gage_years), list(step1),
                                 final_steps='plot', add_complete = FALSE)
   return(task_plan)
@@ -19,8 +44,8 @@ tibble_hash_frames <- function(...){
   tibble(filename = c(...), hash = tools::md5sum(filename))
 }
 
-create_gif_frames <- function(gage_years, width, height, ...){
-  frame_plan <- create_frame_plan(gage_years, width = width, height = height)
+create_gif_frames <- function(gage_years, width, height, format, ...){
+  frame_plan <- create_frame_plan(gage_years, width = width, height = height, format = format)
   
   frame_makefile <- "frame_tasks.yml"
   
@@ -37,3 +62,4 @@ create_gif_frames <- function(gage_years, width, height, ...){
   return(hash_table)
 
 }
+
